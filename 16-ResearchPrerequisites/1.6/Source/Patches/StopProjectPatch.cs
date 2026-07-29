6d73a5e217e7859b017b6fdb1d7ff596a115d414
Sanhu Li <lisanhu2014@hotmail.com>
2026-07-29 23:58:48 +0800

    feat: manual stop clears the corresponding research queue

---
+++ 16-ResearchPrerequisites/1.6/Source/Patches/StopProjectPatch.cs
@@ -0,0 +1,19 @@
+using HarmonyLib;
+using RimWorld;
+using Verse;
+
+namespace ResearchPrerequisites
+{
+    [HarmonyPatch(typeof(ResearchManager), nameof(ResearchManager.StopProject))]
+    public static class StopProjectPatch
+    {
+        public static void Postfix(ResearchProjectDef proj)
+        {
+            if (proj == null)
+            {
+                return;
+            }
+            ResearchQueue.Instance?.ClearQueue(proj.knowledgeCategory);
+        }
+    }
+}
