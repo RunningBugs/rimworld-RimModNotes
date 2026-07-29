db018d9810c52b4076b5d4a43c969dcb7da0b26d
Sanhu Li <lisanhu2014@hotmail.com>
2026-07-29 23:44:10 +0800

    feat: queue button on research panel, ctrl+click jumps to queue front

---
+++ 16-ResearchPrerequisites/1.6/Source/Patches/DrawStartButtonPatch.cs
@@ -0,0 +1,62 @@
+using HarmonyLib;
+using RimWorld;
+using UnityEngine;
+using Verse;
+
+namespace ResearchPrerequisites
+{
+    [HarmonyPatch(typeof(MainTabWindow_Research), "DrawStartButton")]
+    public static class DrawStartButtonPatch
+    {
+        private static readonly AccessTools.FieldRef<MainTabWindow_Research, ResearchProjectDef> SelectedProject =
+            AccessTools.FieldRefAccess<MainTabWindow_Research, ResearchProjectDef>("selectedProject");
+
+        private static Rect queueButtonRect;
+
+        private static bool ShouldShowQueueButton(ResearchProjectDef project)
+        {
+            return project != null && !project.IsFinished && !project.IsHidden
+                && !Find.ResearchManager.IsCurrentProject(project);
+        }
+
+        public static void Prefix(ref Rect startButRect, MainTabWindow_Research __instance)
+        {
+            if (!ShouldShowQueueButton(SelectedProject(__instance)))
+            {
+                return;
+            }
+            queueButtonRect = startButRect.RightPart(0.4f);
+            startButRect = startButRect.LeftPart(0.58f);
+        }
+
+        public static void Postfix(MainTabWindow_Research __instance)
+        {
+            ResearchProjectDef project = SelectedProject(__instance);
+            if (!ShouldShowQueueButton(project))
+            {
+                return;
+            }
+            bool jump = project.CanStartNow
+                && (Input.GetKey(KeyCode.LeftControl) || Input.GetKey(KeyCode.RightControl));
+            string label = jump ? "ResearchJumpQueueFront".Translate() : "ResearchAddToQueue".Translate();
+            TooltipHandler.TipRegion(queueButtonRect, "ResearchQueueButtonTip".Translate());
+            if (Widgets.ButtonText(queueButtonRect, label))
+            {
+                ResearchQueue queue = ResearchQueue.Instance;
+                if (queue == null)
+                {
+                    return;
+                }
+                if (jump)
+                {
+                    queue.JumpToFront(project);
+                }
+                else
+                {
+                    queue.Enqueue(project);
+                }
+                ResearchQueueController.TryStartNext(project.knowledgeCategory);
+            }
+        }
+    }
+}
